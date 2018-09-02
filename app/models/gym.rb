d42a721424a1c71316ead596b1ff11555f7108df
yapjinai <jinai.jyap@gmail.com>
2018-09-02 14:56:59 -0400

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,16 +1,26 @@
 class Gym
  ALL = []
 
-  attr_reader :name
+  attr_reader :name, :memberships, :lifters
 
   def initialize(name)
     @name = name
-    ALL << self
+    @memberships = []
+    @lifters = []
 
+    ALL << self
   end
 
   def self.all
     ALL
+  end
+
+  def lifter_names
+    @lifters.map { |l| l.name }
+  end
+
+  def lift_total_for_gym
+    @lifters.map { |l| l.lift_total }.inject(:+)
   end
 
 end
--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,31 @@
-
 class Lifter
+  ALL = []
 
-
-
-  attr_reader :name, :lift_total
+  attr_reader :name, :lift_total, :memberships, :gyms
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @memberships = []
+    @gyms = []
 
+    ALL << self
   end
 
+  def self.all
+    ALL
+  end
 
+  def self.average_lift
+    ALL.map { |l| l.lift_total }.inject(:+) / ALL.count
+  end
+
+  def sign_up(cost, gym)
+    Membership.new(self, gym, cost)
+  end
+
+  def total_cost
+    @memberships.map { |m| m.cost }.inject(:+)
+  end
 
 end
