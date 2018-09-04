ad9505bc29744d76e56eb5e5d9f797627f7cafd4
github username <github email address>
2018-09-03 21:12:22 -0400

    Done.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,12 +1,20 @@
+require 'pry'
+
 class Membership
+  attr_accessor :lifter , :gym, :cost
 
+  @@all = []
 
-  attr_reader :cost
-
-  def initialize(cost)
+  def initialize(lifter, gym, cost)
     @cost = cost
+    @gym = gym
+    @lifter = lifter
+
+    @@all << self
   end
 
-
+  def self.all
+    @@all
+  end
 
 end
